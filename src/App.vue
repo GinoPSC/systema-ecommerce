6397602a3667832e1b34569bbb912b145d2bdeee
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2022-05-05 20:44:40 -0400

    update navbar with element plus items

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <el-menu
+      :default-active="activeIndex2"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#545c64"
+      text-color="#fff"
+      active-text-color="#ffd04b"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">
+        <el-icon><box /></el-icon>
+        <router-link to="/">Products</router-link>
+      </el-menu-item>
+      <el-menu-item index="2">
+        <el-icon><shoppingCart /></el-icon>
+        <router-link to="/about">Cart</router-link>
+      </el-menu-item>
+    </el-menu>
   </nav>
   <router-view/>
 </template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const activeIndex2 = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+
+import {
+  Box,
+  ShoppingCart as shoppingCart,
+} from '@element-plus/icons-vue'
+const handleOpen = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+const handleClose = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
 
 <style>
 #app {
